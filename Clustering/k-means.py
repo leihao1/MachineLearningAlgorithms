f87475a8fce79ea92e9c617c4e57a49e5b40ec36
Lei Hao <leihao1313@gmail.com>
2018-02-03 04:16:26 -0500

    finish step 2 / 3

--- Clustering/k-means.py
+++ Clustering/k-means.py
@@ -15,35 +15,53 @@ COLUMN=len(lines[0])
 
 back_up={}
 cluster_id=0
-K=0
 all_points=[]
 
+'all_clusters={cluster_id:[x,y,sumx,sumy,N]}'
+all_clusters={}
+
+'good_k={number:avg_cenroid_dis}'
+
 def find_all_points():
+    points=[]
     for r in range(ROW):
         for c in range(COLUMN):
             if lines[r][c] != '':
-                global all_points
-                global back_up
                 position=(r+1,c+1)
-                all_points.append(position)
+                points.append(position)
+                global back_up
                 back_up[position]=int(lines[r][c])
+    return points
 
-def choose_K():
-    return 0
+def select_K():
+    return 4
 
 def a():
-    find_all_points()
-    return pick_initial_points(4)
-
-def pick_initial_points(K):
-    '1.sampling'
-    '2.dispersed'
-    copy=list(all_points)
-    picked=[random.choice(all_points)]
-    print(picked)
-    copy.remove(picked[0])
+    global all_points
+    all_points=find_all_points()
+    global all_clusters
+    all_clusters=pick_initial_points(select_K())
+    print('all_clusters:',all_clusters)
+    assigning_cluster()
+    print('after all_clusters:',all_clusters)
     
-    for n in range(K-1):
+
+'pick K initial points as K clusters by dispersed method'
+def pick_initial_points(K):
+    '  1.sampling'
+    '->2.dispersed'
+
+    copy=list(all_points)
+    print("all_points:",all_points)
+    first=random.choice(all_points)
+    print("first:",first)
+    init_clusters={}
+    init_clusters[0]=[first[0],first[1],first[0],first[1],1]
+
+    copy.remove(first)
+    picked=[first]
+    
+    for n in range(1,K):
         max_distance,waitlist=0,0
         for p in copy:
             distance=0
@@ -51,28 +69,43 @@ def pick_initial_points(K):
                 distance+=math.hypot(p[0]-i[0],p[1]-i[1])
             if distance>max_distance:
                 max_distance,waitlist=distance,p
-        picked.append(waitlist)
-        print(waitlist)
         copy.remove(waitlist)
-
-    return picked 
+        picked.append(waitlist)
+ 
+        init_clusters[n]=[waitlist[0],waitlist[1],waitlist[0],waitlist[1],1]
         
-
-    
-                
+    return init_clusters
         
-    return 0
+'go through the whole dataset and assign each point to one cluster by distance'
+def assigning_cluster():
+    global all_clusters
+    copy=list(all_points)
+    for p in copy:
 
-def classify():
-    return 0
+        cluster_dis=[]
+        for c in all_clusters:
+            dis=math.hypot(p[0]-all_clusters[c][0],p[1]-all_clusters[c][1])
+            cluster_dis.append(dis)
+
+        min_dis,assign=cluster_dis[0],0
+        for i in range(1,len(cluster_dis)):
+            if cluster_dis[i] < min_dis:
+                min_dis,assign=cluster_dis[i],i
+
+        'change sumx and sumy and total points number N'
+        all_clusters[assign][2]+=p[0]
+        all_clusters[assign][3]+=p[1]
+        all_clusters[assign][4]+=1
+
 
 def reset_cluster():
-    if table:
+    if tablelize:
         'next phase'
         return 0
     else:
-        classify()
+        assigning_cluster()
     return 0
 
 def output():
     return 0
+
